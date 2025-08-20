c0f14f7c8b7113670acb5f61e0d3685090c8d05e
Artem <nemezg@gmail.com>
2025-08-20 11:22:47 +0300

    auth verification

---
+++ actions/auth-actions.js
@@ -0,0 +1,39 @@
+"use server";
+
+import { hashUserPassword } from "@/lib/hash";
+import { createUser } from "@/lib/user";
+import { redirect } from "next/navigation";
+
+export async function signup(prevState, formData) {
+  const email = formData.get("email");
+  const password = formData.get("password");
+
+  let errors = {};
+
+  if (!email.includes("@")) {
+    errors.email = "Invalid email address";
+  }
+
+  if (password.trim().length < 6) {
+    errors.password = "Password must be at least 6 characters long";
+  }
+
+  if (Object.keys(errors).length > 0) {
+    return { errors };
+  }
+
+  const hashedPassword = hashUserPassword(password);
+  try {
+    createUser(email, hashedPassword);
+  } catch (error) {
+    if (error.code === "SQLITE_CONSTRAINT_UNIQUE") {
+      return {
+        errors: {
+          email: "Email already exists. Please use a different email address.",
+        },
+      };
+    }
+    throw error;
+  }
+  redirect("/training");
+}
